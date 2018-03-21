c14e3d0434773451cf8c681597c7c897d7e9096f
Zach Davis <zach@castironcoding.com>
2018-03-21 13:48:47 -0700

    [B] On logout, flush all entities from store
    Now that entities have abilities attached to them that are based on the
    current user, we need to remove entities from the store when users
    change. We accomplish this by flushing the models when the user logs
    out. This means that some containers, in particular the Frontend
    container, needs to re-fetch its data after the store has been flushed.
    To accomplish this, the FetchData HOC now exposes a fetchData prop to
    the wrapped component. The Wrapped component can call this at any time
    to trigger a refetch.

--- client/src/containers/frontend/Home.js
+++ client/src/containers/frontend/Home.js
@@ -67,6 +67,7 @@ export class HomeContainer extends Component {
     location: PropTypes.object,
     history: PropTypes.object,
     dispatch: PropTypes.func,
+    fetchData: PropTypes.func.isRequired,
     subjects: PropTypes.array,
     meta: PropTypes.object
   };
@@ -81,6 +82,12 @@ export class HomeContainer extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    if (
+      this.props.authentication.currentUser !==
+      nextProps.authentication.currentUser
+    ) {
+      this.props.fetchData(nextProps);
+    }
     if (nextProps.location !== this.props.location) {
       HomeContainer.fetchProjects(nextProps.dispatch, nextProps.location);
     }
